180ec69e5243103cbe5851ce884c0456b0c82249
Max <maxwellfuraha91@gmail.com>
2024-01-08 15:05:48 +0300

    My first commit using Git bash, To the Java Roadmap repo

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/*This program prints out a welcome message to the user. it just to show
+the basic Java Syntax and how to run a program in Java.*/
+
+// Class name
+public class Main {
+    // Main method
+    public static void main(String[] args) {
+        // Command used to Print "Hello, World" to the terminal window.
+        System.out.println("Hello world, and welcome!");
+
+    }
+}
