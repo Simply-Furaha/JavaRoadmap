a45af0b87754cdfcc527f658bdfad78492c463d3
FURAHA MAXWELL EGESA <146840417+Simply-Furaha@users.noreply.github.com>
2024-01-09 18:15:51 +0300

    Update Main.java
    Included variables and printing statements
    
    Signed-off-by: FURAHA MAXWELL EGESA <146840417+Simply-Furaha@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,11 @@ public class Main {
     public static void main(String[] args) {
         // Command used to Print "Hello, World" to the terminal window.
         System.out.println("Hello world, and welcome!");
-
+        // Lets handle a fe variable and printy them to the terminal
+        int age = 18;
+        string Name = "Mellow"
+            //Printing the Varialble Values
+            System.out.println("Your Age is +" + age); //Prints the value assigned to age variable of Data type integer
+            System.out.println(Name); //Prints the value assigned to Name(Mellow).
     }
 }
