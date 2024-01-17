e61eae011bd9a67496b12a8f3358751403219b29
Max <maxwellfuraha91@gmail.com>
2024-01-17 18:57:37 +0300

    Getting User Input from

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+           Scanner scanner = new Scanner(System.in);
+              int n = scanner.nextInt();
+              String name = scanner.next();
+        System.out.println("Your Age is = " + n);
+        System.out.println("Your Name is = " + name);
+        }
+    }
